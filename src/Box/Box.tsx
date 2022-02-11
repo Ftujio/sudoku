8fbb32b2360dcbd2cbd866a43c92356168ac61a9
Ftujio <svetlin.y8878@gmail.com>
2022-02-11 04:22:09 +0200

    Add selected number on cell click

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useKeyPress } from '../useKeyPress';
 import './Box.scss';
 
 export const Box = (props: BoxProps) => {
+
+	const [value, setValue] = useState<number>();
+	const [selectedNumber, setSelectedNumber] = useState<number>();
+
+	useKeyPress(key => {
+		setSelectedNumber(key);
+	});
+
+	useEffect(() => {
+		if (props.value !== undefined) {
+			setValue(props.value);
+		}
+	}, [props.value]);
+
+	const addNumber = () => {
+		if (selectedNumber) {
+			setValue(selectedNumber);
+		}
+	}
+
 	return (
-		<div className="box box-small">
-			{ props.value !== 0 &&
-				props.value
+		<div className="box box-small" onClick={addNumber}>
+			{ value! !== 0 &&
+				value
 			}
 		</div>
 	);
